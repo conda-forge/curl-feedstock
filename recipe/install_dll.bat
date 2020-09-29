da9e1a0e57fa434cb9e6cc1d520d94c972231811
Filipe Fernandes <ocefpaf@gmail.com>
2020-09-29 12:34:03 -0300

    should still fail b/c we don't want curl in libcurl

--- recipe/install_dll.bat
+++ recipe/install_dll.bat
@@ -9,6 +9,4 @@ if %ARCH% == 32 (
 robocopy ..\builds\libcurl-vc%VS_MAJOR:"=%-%ARCH_STRING%-release-dll-zlib-dll-ssh2-dll-ipv6-sspi-winssl\ %LIBRARY_PREFIX% *.* /E
 if %ERRORLEVEL% GTR 3 exit 1
 
-del /f %LIBRARY_BIN%\curl.exe
-
 exit 0
