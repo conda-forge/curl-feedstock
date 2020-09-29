e7b5dc131fa9eb26a7ef982158a51b4f67bc42a9
Filipe Fernandes <ocefpaf@gmail.com>
2020-09-29 17:19:09 -0300

    test

--- recipe/install_dll.bat
+++ recipe/install_dll.bat
@@ -6,9 +6,11 @@ if %ARCH% == 32 (
     set ARCH_STRING=x64
 )
 
-set BUILD_FOLDER=..\builds\libcurl-vc%VS_MAJOR:"=%-%ARCH_STRING%-release-dll-zlib-dll-ssh2-dll-ipv6-sspi-winssl
+set BUILD_PATH=..\builds\libcurl-vc%VS_MAJOR:"=%-%ARCH_STRING%-release-dll-zlib-dll-ssh2-dll-ipv6-sspi-winssl
 
-robocopy %BUILD_FOLDER%\ %LIBRARY_PREFIX% *.* /E /XF %BUILD_FOLDER%\curl.exe*
+robocopy %BUILD_PATH%\lib %LIBRARY_LIB% *.* /E
+robocopy %BUILD_PATH%\include %LIBRARY_INC% *.* /E
+robocopy %BUILD_PATH%\bin\libcurl.dll %LIBRARY_BIN% *.* /E
 if %ERRORLEVEL% GTR 3 exit 1
 
 exit 0
