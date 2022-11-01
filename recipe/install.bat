f2d674415d2d7823eb1dbff72ff40b08b44f2b92
Wolf Vollprecht <w.vollprecht@gmail.com>
2022-11-01 21:41:15 +0100

    fix up copying of windows files

--- recipe/install.bat
+++ recipe/install.bat
@@ -7,7 +7,7 @@ if %ARCH% == 32 (
 )
 
 REM install static library
-if %PKG_NAME% == "libcurl-static" (
+if "%PKG_NAME%" == "libcurl-static" (
     copy ..\builds\libcurl-vc%VS_MAJOR:"=%-%ARCH_STRING%-release-static-zlib-dll-ssh2-dll-ipv6-sspi-schannel\lib\libcurl_a.lib %LIBRARY_PREFIX%\lib\libcurl_a.lib
     if %ERRORLEVEL% GTR 3 exit 1
 )
@@ -16,8 +16,8 @@ REM install everything else
 robocopy ..\builds\libcurl-vc%VS_MAJOR:"=%-%ARCH_STRING%-release-dll-zlib-dll-ssh2-dll-ipv6-sspi-schannel\ %LIBRARY_PREFIX% *.* /E
 if %ERRORLEVEL% GTR 3 exit 1
 
-if NOT %PKG_NAME% == "curl" (
-    del %LIBRARY_PREFIX%\bin\curl.exe
+if not "%PKG_NAME%" == "curl" (
+    del /f %LIBRARY_PREFIX%\bin\curl.exe
     if %ERRORLEVEL% GTR 3 exit 1
 )
 
