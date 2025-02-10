026dd74f644bd8b39e8ead1aa2460b2b7a62e21e
jakirkham <jakirkham@gmail.com>
2025-02-09 22:14:56 -0800

    Assign reused CMake options to `CMAKE_ARGS`

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -6,22 +6,25 @@ if errorlevel 1 exit 1
 pushd build_%CMAKE_CONFIG%
 if errorlevel 1 exit 1
 
-:: Build and install executable and dynamic libraries
-cmake -G "Ninja" ^
-    %CMAKE_ARGS% ^
+:: Set reused options for CMake builds
+set CMAKE_ARGS=%CMAKE_ARGS% ^
     -D CMAKE_BUILD_TYPE:STRING="%CMAKE_CONFIG%" ^
     -D CMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
     -D CMAKE_POSITION_INDEPENDENT_CODE:BOOL=ON ^
-    -D BUILD_CURL_EXE:BOOL=ON ^
-    -D BUILD_SHARED_LIBS:BOOL=ON ^
-    -D BUILD_STATIC_LIBS:BOOL=OFF ^
     -D CURL_ZLIB:BOOL=ON ^
     -D CURL_USE_LIBPSL:BOOL=OFF ^
     -D CURL_USE_LIBSSH2:BOOL=ON ^
     -D USE_WIN32_IDN:BOOL=OFF ^
     -D CURL_USE_SCHANNEL:BOOL=ON ^
     -D CURL_WINDOWS_SSPI:BOOL=ON ^
-    -D ENABLE_UNICODE:BOOL=ON ^
+    -D ENABLE_UNICODE:BOOL=ON
+
+:: Build and install executable and dynamic libraries
+cmake -G "Ninja" ^
+    %CMAKE_ARGS% ^
+    -D BUILD_CURL_EXE:BOOL=ON ^
+    -D BUILD_SHARED_LIBS:BOOL=ON ^
+    -D BUILD_STATIC_LIBS:BOOL=OFF ^
     "%SRC_DIR%"
 if errorlevel 1 exit 1
 cmake --build . --target install --config "%CMAKE_CONFIG%" -- -v
@@ -30,19 +33,9 @@ if errorlevel 1 exit 1
 :: Build and install static libraries
 cmake -G "Ninja" ^
     %CMAKE_ARGS% ^
-    -D CMAKE_BUILD_TYPE:STRING="%CMAKE_CONFIG%" ^
-    -D CMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
-    -D CMAKE_POSITION_INDEPENDENT_CODE:BOOL=ON ^
     -D BUILD_CURL_EXE:BOOL=OFF ^
     -D BUILD_SHARED_LIBS:BOOL=OFF ^
     -D BUILD_STATIC_LIBS:BOOL=ON ^
-    -D CURL_ZLIB:BOOL=ON ^
-    -D CURL_USE_LIBPSL:BOOL=OFF ^
-    -D CURL_USE_LIBSSH2:BOOL=ON ^
-    -D USE_WIN32_IDN:BOOL=OFF ^
-    -D CURL_USE_SCHANNEL:BOOL=ON ^
-    -D CURL_WINDOWS_SSPI:BOOL=ON ^
-    -D ENABLE_UNICODE:BOOL=ON ^
     "%SRC_DIR%"
 if errorlevel 1 exit 1
 cmake --build . --target install --config "%CMAKE_CONFIG%" -- -v
